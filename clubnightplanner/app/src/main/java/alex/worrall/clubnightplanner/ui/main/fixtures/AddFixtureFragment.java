c5b6cd19b269497d0d7c8672089e16b5b2c7c088
Alex Worrall <a.worrall2512@googlemail.com>
2020-04-25 12:32:12 +0100

    Add time picker

--- clubnightplanner/app/src/main/java/alex/worrall/clubnightplanner/ui/main/fixtures/AddFixtureFragment.java
+++ clubnightplanner/app/src/main/java/alex/worrall/clubnightplanner/ui/main/fixtures/AddFixtureFragment.java
@@ -1,5 +1,6 @@
 package alex.worrall.clubnightplanner.ui.main.fixtures;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,6 +11,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.util.List;
 
@@ -18,6 +21,10 @@ import alex.worrall.clubnightplanner.service.ServiceApi;
 
 public class AddFixtureFragment extends Fragment {
     private ServiceApi service = ServiceApi.getInstance();
+    private boolean initialiseAllCourts = true;
+    private int hr;
+    private int min;
+    private TextView timeOutput;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,12 +42,53 @@ public class AddFixtureFragment extends Fragment {
                 new FixtureCourtSelectorRecyclerViewAdapter(getContext(), data);
         recyclerView.setAdapter(adapter);
         final CheckBox selectAll = rootView.findViewById(R.id.select_all_courts);
+        selectAll.setChecked(initialiseAllCourts);
+        adapter.setCheckedAll(initialiseAllCourts);
         selectAll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 adapter.setCheckedAll(selectAll.isChecked());
             }
         });
+        timeOutput = rootView.findViewById(R.id.fixture_time_output);
+        timeOutput.setText(timeConverter(hr, min));
+        timeOutput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new TimePickerDialog(getContext(), timePickerListener, 0, 0, false).show();
+            }
+        });
         return rootView;
     }
+
+    private TimePickerDialog.OnTimeSetListener timePickerListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            hr = hourOfDay;
+            min = minute;
+            timeOutput.setText(timeConverter(hr, min));
+        }
+    };
+
+    private String timeConverter(int hr, int min) {
+        String am_pm, minutes;
+        int hours;
+        if (hr > 12) {
+            am_pm = " PM";
+            hours = hr - 12;
+        } else if (hr == 0) {
+            am_pm = " AM";
+            hours = 12;
+        } else {
+            am_pm = " AM";
+            hours = hr;
+        }
+        if (min < 10) {
+            minutes = "0" + min;
+        } else {
+            minutes = "" + min;
+        }
+        return hours + ":" + minutes + am_pm;
+    }
+
 }
