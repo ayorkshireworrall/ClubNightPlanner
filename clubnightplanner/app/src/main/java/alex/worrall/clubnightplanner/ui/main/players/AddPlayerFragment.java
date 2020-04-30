5f30d85eee4e2fc54b829c8281c7e2b86f8c3d6d
Alex Worrall <a.worrall2512@googlemail.com>
2020-04-30 22:36:09 +0100

    Style icon and click enter submits player changes

--- clubnightplanner/app/src/main/java/alex/worrall/clubnightplanner/ui/main/players/AddPlayerFragment.java
+++ clubnightplanner/app/src/main/java/alex/worrall/clubnightplanner/ui/main/players/AddPlayerFragment.java
@@ -3,11 +3,14 @@ package alex.worrall.clubnightplanner.ui.main.players;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -41,30 +44,45 @@ public class AddPlayerFragment extends Fragment {
             public void onClick(View v) {
                 String name = nameField.getText().toString();
                 String level = levelField.getText().toString();
-                if (name.isEmpty()) {
-                    String message = "Please fill in the player name field";
-                    Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
-                    toast.show();
-                } else if (level.isEmpty()) {
-                    String message = "Please fill in the player level field";
-                    Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
-                    toast.show();
-                } else if (service.isPlayerNameUsed(name)) {
-                    String message = "Player name " + name +
-                            " already in use, please use another name";
-                    Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
-                    toast.show();
-                } else {
-                    service.addPlayer(name, Integer.parseInt(level));
-                    String message = "Successfully added " + nameField.getText().toString();
-                    Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
-                    toast.show();
-                    Intent parentActivityIntent = getActivity().getParentActivityIntent();
-                    parentActivityIntent.putExtra("TAB_POSITION", TabPositions.PLAYERS);
-                    getActivity().navigateUpTo(parentActivityIntent);
-                }
+                submit(level, context, nameField);
             }
         });
+        levelField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) ||
+                (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    submit(levelField.getText().toString(), context, nameField);
+                }
+                return false;
+            }
+        });
+    }
+
+    private void submit(String level, Context context, EditText nameField) {
+        String name = nameField.getText().toString();
+        if (name.isEmpty()) {
+            String message = "Please fill in the player name field";
+            Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+            toast.show();
+        } else if (level.isEmpty()) {
+            String message = "Please fill in the player level field";
+            Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+            toast.show();
+        } else if (service.isPlayerNameUsed(name)) {
+            String message = "Player name " + name +
+                    " already in use, please use another name";
+            Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+            toast.show();
+        } else {
+            service.addPlayer(name, Integer.parseInt(level));
+            String message = "Successfully added " + nameField.getText().toString();
+            Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+            toast.show();
+            Intent parentActivityIntent = getActivity().getParentActivityIntent();
+            parentActivityIntent.putExtra("TAB_POSITION", TabPositions.PLAYERS);
+            getActivity().navigateUpTo(parentActivityIntent);
+        }
     }
 
 
--- clubnightplanner/app/src/main/java/alex/worrall/clubnightplanner/ui/main/players/EditPlayerFragment.java
+++ clubnightplanner/app/src/main/java/alex/worrall/clubnightplanner/ui/main/players/EditPlayerFragment.java
@@ -2,11 +2,14 @@ package alex.worrall.clubnightplanner.ui.main.players;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -46,29 +49,43 @@ public class EditPlayerFragment extends ObservableFragment {
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = nameField.getText().toString();
-                String level = levelField.getText().toString();
-                if (name.isEmpty()) {
-                    String message = "Please fill in the player name field";
-                    Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
-                } else if (level.isEmpty()) {
-                    String message = "Please fill in the player level field";
-                    Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-                } else if (service.isPlayerNameUsed(name, player.getUuid())) {
-                    String message = "Player name " + name +
-                            " already in use, please use another name";
-                    Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-                } else {
-                    player.setName(name);
-                    player.setLevel(Integer.parseInt(level));
-                    //TODO improve the way that the player is updated (currently this does nothing
-                    // and relies on the fact we are mutating the actual player object!)
-                    service.updatePlayer(player);
-                    notifyObservers();
-                    getActivity().finish();
-                }
+                submit(nameField, levelField, context, player);
             }
         });
+        levelField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER) ||
+                (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    submit(nameField, levelField, context, player);
+                }
+                return false;
+            }
+        });
+    }
+
+    private void submit(EditText nameField, EditText levelField, Context context, Player player) {
+        String name = nameField.getText().toString();
+        String level = levelField.getText().toString();
+        if (name.isEmpty()) {
+            String message = "Please fill in the player name field";
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        } else if (level.isEmpty()) {
+            String message = "Please fill in the player level field";
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+        } else if (service.isPlayerNameUsed(name, player.getUuid())) {
+            String message = "Player name " + name +
+                    " already in use, please use another name";
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+        } else {
+            player.setName(name);
+            player.setLevel(Integer.parseInt(level));
+            //TODO improve the way that the player is updated (currently this does nothing
+            // and relies on the fact we are mutating the actual player object!)
+            service.updatePlayer(player);
+            notifyObservers();
+            getActivity().finish();
+        }
     }
 
 
