19f78361cbf0fabdf4298c07640211ffff195040
Alex Worrall <a.worrall2512@googlemail.com>
2020-05-17 16:59:59 +0100

    Look and feel changes to player form

--- clubnightplanner/app/src/main/java/alex/worrall/clubnightplanner/ui/main/players/AddPlayerActivity.java
+++ clubnightplanner/app/src/main/java/alex/worrall/clubnightplanner/ui/main/players/AddPlayerActivity.java
@@ -2,9 +2,13 @@ package alex.worrall.clubnightplanner.ui.main.players;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,33 +32,48 @@ public class AddPlayerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         playerRepository = new PlayerRepository(getApplication());
         setContentView(R.layout.activity_add_player);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
         nameText = findViewById(R.id.player_form_name);
         levelText = findViewById(R.id.player_form_level);
+        levelText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) ||
+                        (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    submit();
+                }
+                return false;
+            }
+        });
         Button submitButton = findViewById(R.id.submit_player_form);
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = nameText.getText().toString();
-                if (name.isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Please add a non empty player name",
-                            Toast.LENGTH_SHORT).show();
-                } else if (isNameUsed(name)) {
-                    Toast.makeText(getApplicationContext(), "The name " + name +
-                                    " is already in use, please choose another name",
-                            Toast.LENGTH_SHORT).show();
-                } else if (levelText.getText().toString().isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Please add a player level",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    Intent replyIntent = new Intent();
-                    replyIntent.putExtra(EXTRA_NAME, name);
-                    replyIntent.putExtra(EXTRA_LEVEL, levelText.getText().toString());
-                    setResult(RESULT_OK, replyIntent);
-                    finish();
-                }
+                submit();
             }
         });
+    }
+
+    private void submit() {
+        String name = nameText.getText().toString();
+        if (name.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please add a non empty player name",
+                    Toast.LENGTH_SHORT).show();
+        } else if (isNameUsed(name)) {
+            Toast.makeText(getApplicationContext(), "The name " + name +
+                            " is already in use, please choose another name",
+                    Toast.LENGTH_SHORT).show();
+        } else if (levelText.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please add a player level",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Intent replyIntent = new Intent();
+            replyIntent.putExtra(EXTRA_NAME, name);
+            replyIntent.putExtra(EXTRA_LEVEL, levelText.getText().toString());
+            setResult(RESULT_OK, replyIntent);
+            finish();
+        }
     }
 
     private boolean isNameUsed(String name) {
--- clubnightplanner/app/src/main/java/alex/worrall/clubnightplanner/ui/main/players/EditPlayerActivity.java
+++ clubnightplanner/app/src/main/java/alex/worrall/clubnightplanner/ui/main/players/EditPlayerActivity.java
@@ -2,9 +2,12 @@ package alex.worrall.clubnightplanner.ui.main.players;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -37,32 +40,47 @@ public class EditPlayerActivity extends AppCompatActivity {
         nameText.setText(player.getName());
         levelText = findViewById(R.id.player_form_level);
         levelText.setText(String.valueOf(player.getLevel()));
+        levelText.setSelectAllOnFocus(true);
+        levelText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) ||
+                        (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    submit(player);
+                }
+                return false;
+            }
+        });
         Button submitButton = findViewById(R.id.submit_player_form);
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = nameText.getText().toString();
-                if (name.isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Please add a non empty player name",
-                            Toast.LENGTH_SHORT).show();
-                } else if (!name.equals(player.getName()) && isNameUsed(name)) {
-                    Toast.makeText(getApplicationContext(), "The name " + name +
-                                    " is already in use, please choose another name",
-                            Toast.LENGTH_SHORT).show();
-                } else if (levelText.getText().toString().isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Please add a player level",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    Intent replyIntent = new Intent();
-                    player.setLevel(Integer.parseInt(levelText.getText().toString()));
-                    player.setName(name);
-                    replyIntent.putExtra(MainActivity.EXTRA_PLAYER, player);
-                    setResult(RESULT_OK, replyIntent);
-                    finish();
-                }
+                submit(player);
             }
         });
+    }
+
+    private void submit(Player player) {
+        String name = nameText.getText().toString();
+        if (name.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please add a non empty player name",
+                    Toast.LENGTH_SHORT).show();
+        } else if (!name.equals(player.getName()) && isNameUsed(name)) {
+            Toast.makeText(getApplicationContext(), "The name " + name +
+                            " is already in use, please choose another name",
+                    Toast.LENGTH_SHORT).show();
+        } else if (levelText.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please add a player level",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Intent replyIntent = new Intent();
+            player.setLevel(Integer.parseInt(levelText.getText().toString()));
+            player.setName(name);
+            replyIntent.putExtra(MainActivity.EXTRA_PLAYER, player);
+            setResult(RESULT_OK, replyIntent);
+            finish();
+        }
     }
 
     private boolean isNameUsed(String name) {
